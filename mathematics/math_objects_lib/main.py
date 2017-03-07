2eadd25e17efe435fc27601f67995cbcb8b388b4
all-hack <obelanger@fordham.edu>
2017-03-07 04:08:25 -0800

    D

--- mathematics/math_objects_lib/main.py
+++ mathematics/math_objects_lib/main.py
@@ -1,8 +1,27 @@
 
 from matrix import Matrix
 
+    #    1 2 3 4 5 6 7
+l = [   
+        [1,2,3,4,5,6,7], # 1 
+        [1,2,3,4,5,6,7], # 2
+        [1,2,3,4,5,6,7], # 3
+        [1,2,3,4,5,6,7], # 4
+        [0,0,0,0,0,0,0], # 5
+        [0,0,0,0,0,0,0], # 6        
+        [0,0,0,0,0,0,0]  # 7
+    ]
 
-l = [[1,0,0], [0,1,0], [0,0,1]]
+l = [   
+        [1,5,2,3,6,4,7], # 1 
+        [0,0,0,0,0,0,0], # 5
+        [1,5,2,3,6,4,7], # 2        
+        [1,5,2,3,6,4,7], # 3
+        [0,0,0,0,0,0,0], # 6        
+        [1,5,2,3,6,4,7], # 4                
+        [0,0,0,0,0,0,0]  # 7
+    ]
+
 
 
 matrix = Matrix(l)
@@ -11,8 +30,17 @@ m = matrix.x
 
 
 print matrix
-
-
+# print ""
+# print matrix.stable_len
+# matrix.canonical_form()
+# print ""
+# matrix.push_down_row(matrix.index_to_push())
+# matrix.proper_order()
+matrix.canonical_form()
+print matrix
+# print matrix.verify_form()
+# matrix.verify_form()
+print ""
 
 
 
--- mathematics/math_objects_lib/matrix.py
+++ mathematics/math_objects_lib/matrix.py
@@ -1,7 +1,5 @@
 
-
-
-
+from fractions import Fraction as ff
 
 
 class Matrix(object):
@@ -10,20 +8,10 @@ class Matrix(object):
 		self.x = array2d
 		self.len = len(array2d)
 		self.ident = self.identity()
-
+		self.stable_len = self.terminals_len()
 
 	def __str__(self):
-		s = ""
-		s += "[\n"
-		for x in self.x:
-			s += "  ["
-			s += "  "
-			for y in x:		
-				s += str(y)
-				s += ", "
-			s += "],\n"
-		s += "]"
-		return s	
+		return self.to_str()
 
 	def identity(self):
 		top = [] 				
@@ -36,12 +24,88 @@ class Matrix(object):
 
 		return top
 
+	def to_str(self, arr=None):
+		s = ""
+		s += "[\n"
+
+		count = 0
+		if not arr:
+			arr = self.x
+
+		for x in arr:
+			s += "  ["
+			s += "  "
+			for y in x:		
+				s += str(y)
+				s += ", " 				
+			s += "], "+ str(count)+"\n"
+			count += 1
+		s += "]"
+		return s
 
 
+	def canonical_form(self):
+		self.proper_order()
+		self.proper_form()
 
 
+	def proper_form(self):
+		for i, e in enumerate(self.x):
+			if e == [0] * self.len:
+				self.x[i][i] = 1
+			else:
+				self.fractionize(i)
+
+	def proper_order(self):		
+		while not self.verify_form():
+			index = self.index_to_push()
+			self.shift_right(index)
+			self.push_down_row(index)
+
+	def fractionize(self, index):
+		total = 0
+		for e in self.x[index]:
+			total += e
+		for i, e in enumerate(self.x[index]):
+			self.x[index][i] = ff(e, total)
+
+	def index_to_push(self):
+		well_formed = True
+		for i, e in enumerate(reversed(self.x)):			
+			if e != [0] * self.len:			
+				well_formed = False
+			if e == [0] * self.len and well_formed == False:				
+				return (self.len - i - 1)
+
+	def shift_right(self, index):		
+		for e in self.x:
+			tmp = e[index + 1]
+			e[index + 1] = e[index]
+			e[index] = tmp
+
+	def push_down_row(self, index):
+		tmp = self.x[index+1]
+		self.x[index+1] = self.x[index]
+		self.x[index] = tmp
+
+	def verify_form(self):		
+		arr = self.x[self.len - self.stable_len:]
+		count = 0		
+		for e in arr:
+			if e == [0] * self.len:
+				count += 1
+		if count == self.stable_len:
+			return True
+		else:
+			return False
 
 
+	def terminals_len(self):
+		stable = 0
+		for e in self.x:
+			if e == [0] * self.len:
+				stable += 1
+		return stable
 
 
 
